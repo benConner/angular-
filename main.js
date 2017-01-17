674806b93df3e7026815d5d98c67486c98612c50
Ben Conner <ben.conner40@gmail.com>
2017-01-17 15:27:33 -0600

    added detail.html to show li in detail

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 var app = angular.module('appsForDayz', ['ngRoute'])
 
-app.config(($routeProvider)=>{
+app.config(($routeProvider, $locationProvider)=>{
+    $locationProvider.hashPrefix('')
     $routeProvider
         .when('/', {
-                controller: 'MainCtrl',
-                templateUrl: 'partials/main.html'
+            controller: 'MainCtrl',
+            templateUrl: 'partials/main.html'
         }).when('/list',{
             controller: 'ListCtrl',
             templateUrl:'partials/list.html'
+        }).when('/list/:someVarible',{
+            controller: 'DetailCtrl',
+            templateUrl: 'partials/detail.html'
+        }).otherwise({
+            redirectTo:'/'
         })
 })
 
 app.controller('MainCtrl', function($scope){
     $scope.appData = "Hi apps For Dayz here!!"
+    $scope.functionThing = function () {
+        console.log($scope.textbox)
+    }
 })
 
 app.controller(`ListCtrl`, function($scope, $http){
@@ -20,4 +29,14 @@ app.controller(`ListCtrl`, function($scope, $http){
     .then((val) =>{
         $scope.list = val.data.list
     })
+})
+
+app.controller(`DetailCtrl`, function($scope, $routeParams, $http){
+    $scope.thingsToForm = $routeParams.someVarible
+    $http.get(`list.json`)
+    .then((val)=>{
+        let list = val.data.list
+        $scope.selItm = list[$scope.thingsToForm]
+    })
+
 })
